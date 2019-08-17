34e69973180af54aeb77fb7261a9288039e329b9
jal2018 <39078314+jal2018@users.noreply.github.com>
2019-08-17 11:45:42 +0800

    add y8.com.ts

--- spiders/scripts/y8.com.ts
+++ spiders/scripts/y8.com.ts
@@ -1,8 +1,3 @@
-/**
-* 涉及到的知识点
-* 1、没哈，原来有的难题这回都没有了
-* 2、获取页面元素要进行判断，不能让任务中止
-*/
 import * as puppeteer from 'puppeteer'
 import * as fileSystem from 'fs'
 import * as path from 'path'
@@ -11,95 +6,172 @@ import { timeout } from '../utils/timeout'
 
 const log = console.log
 
-interface CommentData {
-score: string
-commentDetail: string
-time: string
+interface GameData {
+	title: string
+	img: string
+	url: string
+	desc: string
+	tags: string
+	cat: string
+	date: string
+	played: number
+	gametype: string
 }
 
+const filePath = path.resolve(__dirname, '../data/y8.com/' + 'y8.json');
+var pageCount = 0;
+var category="";
 var browser;
-
-async function fetchgame (url): Promise<void> {
-	const page = await browser.newPage()
-	try {
-		await page.goto(url);
-		console.log(url)
-		
-		const list = await page.evaluate(() => {
-			//unity
-			let iframe = document.querySelector("#html5-content")
-			if(iframe == null){
-				let itemRoot = document.querySelector(".item-root")
-			}
-		})
-	} catch (error) {
-		console.log(error)
-		log(chalk.red('服务意外终止'))
-	} finally {
-		page.close();
+const [node, tsPath, startPage, ...args] = process.argv;
+/读数据文件*/
+function readJson(jsonFilePath: string) {
+	if (fileSystem.existsSync(jsonFilePath)) {
+		return JSON.parse(fileSystem.readFileSync(jsonFilePath, 'utf-8'));
+	}
+	else {
+		return {};
 	}
 }
+/*初始化上次抓取进度*/
+const ResultData = readJson(filePath);
+const gamesDatas = ResultData.data;
+const indexDatas = ResultData.index;
+var nextIndex = Object.keys(indexDatas).length;
+var pageNumber = (nextIndex >> 6) + 1;
+console.log("初始页码-----------" + pageNumber);
 
-async function main (): Promise<void> {
+async function main(): Promise<void> {
 	/*实例化浏览器对象*/
 	browser = await puppeteer.launch({
 		headless: false,
-		timeout: 100000,
-		args:['--proxy-server=127.0.0.1:8080', '--ignore-certificate-errors'],
+		timeout: 30000,
+		//args:['--proxy-server=127.0.0.1:8080', '--ignore-certificate-errors'],
 		ignoreHTTPSErrors: true
 	})
 	log(chalk.green('服务正常启动'))
 
 	/*开始抓取数据*/
 	try {
-	/*实例化页面*/
+		/*实例化页面*/
 		const page = await browser.newPage()
 		await page.setViewport({
 			width: 1366,
 			height: 768
 		})
-		page.on('console', msg => {
-			if (typeof msg === 'object') {
-				console.dir(msg)
-			} else {
-				log(chalk.blue(msg))
-			}
-		})
 
 		/*起始页面*/
-		await page.goto('https://zh.y8.com/categories/girls?page=100')
-		log(chalk.yellow('页面初次加载完毕'))
-
-		let aTags = await page.evaluate(() => {
-			let as = [...document.querySelectorAll('.thumbarea>a')];
-			console.log('....... .......  ======= .......')
-			console.log(as.length)
-			return as.map((a) =>{
-				console.log('....... .......  ======= .......')
-				console.log(a)
-				return {
-					href: a['href'].trim(),
-					name: a['text']
-				}
-			});
+		await page.goto(startPage, { timeout: 0 });
+		log(chalk.yellow('页面初次加载完毕'));
+		pageCount=await page.evaluate(()=>{
+			let lastHref=document.querySelector(".last.long").getAttribute("href").trim();
+			return parseInt(lastHref.substring(lastHref.indexOf('=')+1));
 		});
-		for (var i = 1; i < aTags.length; i++) {
-			var a = aTags[i];
-			await fetchgame(a.href);
-			await timeout(2000);
-		}
+		category=await page.evaluate(()=>{
+			return document.querySelector(".small-title.with-description").textContent.trim();
+		})
+		console.log("total pages:"+pageCount);
+		console.log("game category:"+category);
+		const handleData = async () => {
+			let dataArray = await page.evaluate((idata) => {
+				const gamesData: GameData[] = [];
+				let gameNode = document.querySelectorAll('.item.thumb.videobox');
+				for (let i = 0; i < gameNode.length; ++i) {
+					let gameData: GameData = {
+						title: undefined,
+						img: undefined,
+						url: undefined,
+						desc: undefined,
+						tags: undefined,
+						cat: "女生游戏",
+						date: undefined,
+						played: undefined,
+						gametype: undefined
+					};
+					let title = gameNode[i].querySelector(".title.ltr");
+					gameData.title = title.innerHTML.trim();
+					//过滤已经存在的title
+					if (idata.hasOwnProperty(gameData.title))
+						continue;
+					gameData.tags = gameNode[i].getAttribute("data-label-ids").trim();
 
+					let imgTag = gameNode[i].querySelector("img");
+					gameData.img = imgTag.src;
+					/*此处为进入当前game的url*/
+					let aTag = gameNode[i].querySelector("a");
+					gameData.url = aTag.href;
+
+					gameData.date = new Date().toLocaleString();
+
+					let playCounts = gameNode[i].querySelector(".plays-count");
+					gameData.played = parseInt(playCounts.innerHTML.trim().substring(0, playCounts.innerHTML.trim().indexOf('次')).replace(/,/g, ''));
+
+					let gameType = gameNode[i].querySelector(".technology>p");
+					gameData.gametype = gameType.innerHTML.trim();
+
+					gamesData.push(gameData);
+				}
+				return gamesData;
+			}, indexDatas);
+			for (let i = 0; i < dataArray.length; ++i, ++nextIndex) {
+				/*跳转到对应game页面抓取数据*/
+				await page.goto(dataArray[i].url, { timeout: 0 });
+				console.log(dataArray[i].url);
+				/*game描述信息*/
+				dataArray[i].desc = await page.evaluate(() => {
+					let d = document.querySelector(".ltr.description");
+					if (d)
+						return d.textContent.trim();
+					else {
+						console.log("---------------------");
+						return "";
+					}
+				});
+				/*根据gametype抓取url*/
+				if (dataArray[i].gametype == "Flash Game") {
+					dataArray[i].url = await page.evaluate(() => {
+						return (document.querySelector("#gamefileEmbed") as HTMLEmbedElement).src;
+					});
+				}
+				else if (dataArray[i].gametype == "HTML5 Game") {
+					dataArray[i].url = await page.evaluate(() => {
+						return (document.querySelector("#html5-content") as HTMLIFrameElement).src;
+					});
+				}
+				else if (dataArray[i].gametype == "Unity WebGL Game") {
+					dataArray[i].url = await page.evaluate(() => {
+						return (document.querySelector("#html5-content") as HTMLIFrameElement).src;
+					});
+				}
+				/*整理数据写入文件*/
+				gamesDatas.push(dataArray[i]);
+				indexDatas[dataArray[i].title] = nextIndex;
+				ResultData["data"] = gamesDatas;
+				ResultData["index"] = indexDatas;
+				fileSystem.writeFile(filePath, JSON.stringify(ResultData), {}, function (err) {
+					if (err) {
+						log(chalk.red('写入文件失败'));
+					} else {
+						log(chalk.yellow('写入文件成功'));
+					}
+				});
+			}
+		};
+		/*从起始页循环抓取数据*/
+		for (let i = pageNumber; i <= pageCount; ++i) {
+			await page.goto(startPage+"?page=" + i, { timeout: 0 });
+			console.log("进入第"+pageNumber+"页")
+			await handleData();
+			await page.waitFor(3000);
+		}
 		/*任务结束，关闭浏览器对象*/
-		await browser.close()
-		log(chalk.green('服务正常结束'))
+		await browser.close();
+		log(chalk.green('服务正常结束'));
 	} catch (error) {
-		console.log(error)
-		log(chalk.red('服务意外终止'))
+		console.log(error);
+		log(chalk.red('服务意外终止'));
 	} finally {
 		//process.exit(0)
 	}
 }
-
-/*调用方法*/
-// noinspection JSIgnoredPromiseFromCall
+/*程序入口*/
 main()
