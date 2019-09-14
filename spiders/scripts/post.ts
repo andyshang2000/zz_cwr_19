4c2ffbd3328c40593738e5b927e8860041c7a0e1
jal2018 <894694305@qq.com>
2019-09-14 14:54:23 +0800

    post.ts可以上传, bug:game category需要先上传

--- spiders/scripts/post.ts
+++ spiders/scripts/post.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import {timeout} from '../utils/timeout'
+import { timeout } from '../utils/timeout'
 
-var request =require("request")
+var request = require("request")
 
 interface uploadLog {
     gameName: string
@@ -11,111 +11,131 @@ interface uploadLog {
 }
 
 interface gamedata {
-	title: string
-	img: string
-	url: string
-	desc: string
-	tags: string
-	cat: string
-	date: string
-	played: number
-	gametype: string
+    title: string
+    img: string
+    url: string
+    desc: string
+    tags: string
+    cat: string
+    date: string
+    played: number
+    gametype: string
 }
 
-interface postdata{
-    name: string
-    imageurl: string
-    gameurl: string
-    width:number
-    height:number
-    type: string
-    rating:number
-    plays:number,
-    desc:string
-    category_id:string
-    localimg:string
-    localgame:string
-    };
+interface postdata {
+    name: string
+    imageurl: string
+    gameurl: string
+    width: number
+    height: number
+    type: string
+    rating: number
+    plays: number,
+    desc: string
+    category_id: number
+    localimg: string
+    localgame: string
+};
 
 
 function readJson(jsonFilePath: string) {
-	if (fs.existsSync(jsonFilePath)) {
-		let content = fs.readFileSync(jsonFilePath, 'utf-8');
-		if (content)
-			return JSON.parse(content);
-		return null;
-	}
-	else {
-		return null;
-	}
+    if (fs.existsSync(jsonFilePath)) {
+        let content = fs.readFileSync(jsonFilePath, 'utf-8');
+        if (content)
+            return JSON.parse(content);
+        return null;
+    }
+    else {
+        return null;
+    }
 }
-var gamesData:gamedata[]=[];
+var catArray = ['Girls'];
+var gamesData: gamedata[] = [];
 const filePath = path.resolve(__dirname, 'E:/desktop/Main/spiders/data/up.zdhm.xyz/all.json');
-gamesData=readJson(filePath).data;
+gamesData = readJson(filePath).data;
 
-var logData:uploadLog[]=[];
-const uploadLogPath=path.resolve(__dirname,'../data/upload.log');
+var logData: uploadLog[] = [];
+const uploadLogPath = path.resolve(__dirname, '../data/upload.log');
 logData = readJson(uploadLogPath).uploadLog;
-var json={};
-for(let i=0;i<logData.length;++i){
-     let title=logData[i].gameName;
-    if(title.indexOf("-")!=-1){
+var json = {};
+for (let i = 0; i < logData.length; ++i) {
+    let title = logData[i].gameName;
+    if (title.indexOf("-") != -1) {
         let arr = title.split("-");
-        for(let j=0;j<arr.length;++j){
-            arr[j]=arr[j].charAt(0).toUpperCase()+arr[j].slice(1);
+        for (let j = 0; j < arr.length; ++j) {
+            arr[j] = arr[j].charAt(0).toUpperCase() + arr[j].slice(1);
         }
-        json[arr.join(" ")]=logData[i].path;
+        json[arr.join(" ")] = logData[i].path;
     }
-    else if(title.indexOf("_")!=-1){
+    else if (title.indexOf("_") != -1) {
         let arr = title.split("_");
-        for(let j=0;j<arr.length;++j){
-            arr[j]=arr[j].charAt(0).toUpperCase()+arr[j].slice(1);
+        for (let j = 0; j < arr.length; ++j) {
+            arr[j] = arr[j].charAt(0).toUpperCase() + arr[j].slice(1);
         }
-        json[arr.join(" ")]=logData[i].path;
-    }else{
-        json[title.toUpperCase()]=logData[i].path;
+        json[arr.join(" ")] = logData[i].path;
+    } else {
+        json[title.toUpperCase()] = logData[i].path;
     }
 }
 
-for(let i=0;i<gamesData.length;++i){
-    let data:postdata={
-        name:gamesData[i].title,
-        imageurl:gamesData[i].img,
-        gameurl:gamesData[i].url,
-        width: 800,
-        height: 600,
-        type:gamesData[i].gametype,
-        rating:1,
-        plays:1,
-        desc:gamesData[i].desc,
-        category_id:'1',
-        localimg:"",
-        localgame:""
+for (let i = 0; i < gamesData.length; ++i) {
+    let data: postdata = {
+        name: gamesData[i].title,
+        imageurl: gamesData[i].img,
+        gameurl: gamesData[i].url,
+        width: 800,
+        height: 600,
+        type: gamesData[i].gametype,
+        rating: 1,
+        plays: 1,
+        desc: gamesData[i].desc,
+        category_id: 1,
+        localimg: "",
+        localgame: ""
     }
-    if(gamesData[i].played){
-        data.plays=gamesData[i].played;
+    if (gamesData[i].played) {
+        data.plays = gamesData[i].played;
     }
-    if(gamesData[i].cat){
-        data.category_id=gamesData[i].cat;
+    if (gamesData[i].cat) {
+        //data.category_id=gamesData[i].cat;
+        var catdata = {
+            name: gamesData[i].cat,
+            rating: 1
+        }
+        if (catArray.indexOf(gamesData[i].cat) == -1) {
+            catArray.push(gamesData[i].cat);
+            catdata.rating = catArray.indexOf(gamesData[i].cat) + 1
+            data.category_id = catdata.rating
+            request.post({
+                url: "http://www.jzjo.com/admin/categories/create",
+                method: 'POST',
+                form: catdata
+            }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log("add cat 成功");
+                }
+            });
+        }
+
     }
-    if(!json[gamesData[i].title])
+    if (!json[gamesData[i].title])
         continue;
-    var arr=gamesData[i].title.split(" ");
-    for(let j=0;j<arr.length;++j){
-        arr[j]=arr[j].charAt(0).toLowerCase()+arr[j].slice(1);
+    var arr = gamesData[i].title.split(" ");
+    for (let j = 0; j < arr.length; ++j) {
+        arr[j] = arr[j].charAt(0).toLowerCase() + arr[j].slice(1);
     }
-    data.localgame=json[gamesData[i].title];
-    data.localimg="/gamesimages/"+arr.join("-");
+    data.localgame = json[gamesData[i].title];
+    data.localimg = "/gamesimages/" + arr.join("-");
 
     request.post({
-        url:"https://www.jzjo.com/post.php",
-        form:data
-    },function(error,response,body){
-        if(!error&&response.statusCode == 200){
+        url: "https://www.jzjo.com/post.php",
+        method: 'POST',
+        form: data
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
             console.log("上传成功");
         }
     });
-    timeout(500);
 }
 
 
