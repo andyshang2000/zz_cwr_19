ffe53213539f140478aada02115d69d0e8660e71
ands <505911494@qq.com>
2019-09-18 15:56:51 +0800

    传数据和下载图片采用递归实现同步

--- spiders/scripts/downloadImg.ts
+++ spiders/scripts/downloadImg.ts
@@ -1,7 +1,5 @@
 import * as fs from 'fs'
 import * as path from 'path'
-import { download } from "./download_swf"
-
 interface gamedata {
     title: string
     img: string
@@ -29,17 +27,38 @@ function readJson(jsonFilePath: string) {
 var gamesData: gamedata[] = [];
 const filePath = path.resolve(__dirname, 'E:/desktop/Main/spiders/data/up.zdhm.xyz/all.json');
 gamesData = readJson(filePath).data;
-for(let i=0;i<gamesData.length;++i){
-    var arr=gamesData[i].title.split(" ")
-    for(let j=0;j<arr.length;++j){
-        arr[j]=arr[j].toLowerCase();
-    }
-    var imgName=arr.join("-")+".png";
-    try {
-        download(gamesData[i].img,"../data/xyz.games/"+imgName);
-    } catch (error) {
-        console.log(error.message);
-    }
+
+function getImg(index){
+	if(index>=gamesData.length)return
+
+	var arr=gamesData[index].img.split("/")
+    var imgName=arr[arr.length-1];
+	console.log(imgName)
+	if(imgName.indexOf("png")!=-1||imgName.indexOf("jpg")!=-1||imgName.indexOf("jpeg")!=-1){
+			var request=require("request");
+		let writeStream = fs.createWriteStream("../data/xyz.games/"+imgName);
+		let readStream = request(gamesData[index].img,(err,response)=>{
+			if(err){
+				console.log("错误信息:" + err)
+				process.exit(1);
+			}
+		});
+		readStream.pipe(writeStream);
+		
+		readStream.on('end', function () {
+			console.log('文件下载成功');
+			getImg(index+1);
+		});
+	}
+	else {
+		getImg(index+1);
+	}
+		
+
 
 }
+
+getImg(0)
+
+
 //download("http://up.zdhm.xyz/images/2018102409434778511.png","../data/1.png")
